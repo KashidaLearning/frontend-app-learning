d1700760b6d6b03bf186f8246b2f74b61c46c8f4
Harout Mardirossian <harmardir@gmail.com>
2025-11-21 15:11:44 +0400

    editing styles

--- src/courseware/course/sidebar/sidebars/course-outline/components/SidebarUnit.jsx
+++ src/courseware/course/sidebar/sidebars/course-outline/components/SidebarUnit.jsx
@@ -26,7 +26,7 @@ const SidebarUnit = ({
   const iconType = isLocked ? UNIT_ICON_TYPES.lock : icon;
 
   return (
-    <li className={classNames({ 'bg-info-100': isActive, 'border-top border-light': !isFirst })}>
+    <li className={classNames({ 'bg-info-100': isActive, 'custom-border-top': !isFirst })}>
       <UnitLinkWrapper
         {...{
           sequenceId,
